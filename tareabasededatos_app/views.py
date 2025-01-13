72bd88e3b7895f68d44930bfd36a26f0f94fe04f
MoralesMortales <moralesyaguilera@gmail.com>
2025-01-12 22:48:16 -0400

    sdori

--- tareabasededatos_app/views.py
+++ tareabasededatos_app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import redirect, render
+from .models import Employee
+from .forms import EmployeeForm
 
-# Create your views here.
 def mainview(request):
     employees = Employee.objects.all()
-    return render(request, 'index.html', {'employees':employees})
+    form_add = EmployeeForm()
+
+    if request.method == "POST":
+        form_add = EmployeeForm(request.POST)
+        if form_add.is_valid():
+            form_add.save()
+            print('Guardado')
+            return redirect('index')
+        else:
+            print('No se guardó')
+
+    return render(request, 'index.html', {'employees': employees, 'form_add': form_add})
