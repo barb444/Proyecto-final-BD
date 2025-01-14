2d5cbcc8bdb0310400a303072affa0261d191416
MoralesMortales <moralesyaguilera@gmail.com>
2025-01-14 08:47:02 -0400

    FEUER 2.0

--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -14,20 +14,43 @@ function canBtnF(){
     document.getElementById("filPp").style.display = "none";
 }
 
+let form_d = ''
+let cedula = 0
 
-function delpp(){
-    document.getElementById("delPp").style.display = "flex";
-  const form = document.getElementsByClassName("deleteForm" + cedula);
-  form.action = `${cedula}/`;
+function delpp(cedula_n){
+  document.getElementById("delPp").style.display = "flex";
+  form_d = document.getElementsByClassName("deleteForm" + cedula_n);
+  form_d = form_d[0]
+  console.log(cedula_n, ' n')
+  cedula = cedula_n
+  console.log(cedula, '-!')
+ }
 
+function delete_now(){
+  event.preventDefault()
+  console.log('running ', cedula)
+  console.log(form_d)
+  form_d.action = `${cedula}/`
+  form_d.submit()
 }
+
 function canBtnD(){
     document.getElementById("delPp").style.display = "none";
 }
 
 
-function editpp(){
-    document.getElementById("editPp").style.display = "flex";
+let form_e = ''
+let cedula_e = 0
+
+function editpp(cedula_n) {
+  // Mostrar el popup
+  document.getElementById("editPp").style.display = "flex";
+  
+  // No es necesario modificar los valores de los campos
+  // Solo mostramos el formulario de edición.
+  
+  // Si deseas hacer algo más, como almacenar la cédula para enviar con el formulario, puedes hacerlo aquí.
+  cedula_e = cedula_n;
 }
 function canBtnE(){
     document.getElementById("editPp").style.display = "none";
